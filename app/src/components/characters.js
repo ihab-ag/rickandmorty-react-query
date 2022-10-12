42040c55066f15a636e58f285d5e9210f3748206
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-12 22:20:25 +0300

    feat: get characters by use query

--- app/src/components/characters.js
+++ app/src/components/characters.js
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { useQuery } from 'react-query';
 export function Characters() {
 
-    const [characters,setCharacters] = useState([]);
 
     const fetchCharacters = async () =>{
         const response = await fetch("https://rickandmortyapi.com/api/character");
-        const data = await response.json();
-        setCharacters(data.results);
+        return response.results.json();
     }
-    useEffect(() => {
-      fetchCharacters();
-    })
+
+    const {data: characters, status} = useQuery("characters",fetchCharacters);
     
-    
+    if(status === "loading"){
+    return  <div>Loading</div> }
+
   return (
     <>
     {characters.map(character=> <div>{character.name}</div>)}

3da2bdde757abd774a3e1d5c96286c2ec3c12636
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-12 22:37:43 +0300

    config: react query client

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import { Characters } from './components/characters';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
+    <QueryClientProvider client={queryClient} >
+    <ReactQueryDevtools initialIsOpen />
     <Characters />
+    </QueryClientProvider>
     </>
   );
 }
